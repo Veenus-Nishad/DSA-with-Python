d12ee5ee247cfd1c2fb50b5c562e132be16fad7c
veenus nishad <veenusnishad@proton.me>
2024-07-24 22:57:30 +0530

    24 july

--- Problems/Rotate Array by K.py
+++ Problems/Rotate Array by K.py
@@ -2,7 +2,7 @@ class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         temp=[None]*len(nums) # creating empty array so that values are not overwritten by logic
         for x in range(len(nums)):
-            newIndex=(k+x)%len(nums)
+            newIndex=(k+x)%len(nums) # Main Formula as anything by % comes between [0 to 9]
             temp[newIndex]=nums[x]
         nums[:]=temp[:]
         
